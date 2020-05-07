00678c145f7c8c3164dcb57dd4648c3ec1fad24f
Atorres2001 <64925159+Atorres2001@users.noreply.github.com>
2020-05-07 16:46:42 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,13 +4,22 @@
  */
 public class Player {
 	//hand of player that will holds the cards
-	Hand hand;
+	private Hand hand;
+	
 	
 	//constructor that receives an hand
 	public Player(Hand h) {
 		hand = h;
 	}
 	
+	//returns hand
+	public Hand getHand() {
+		return hand;
+	}
+	
+	public int getHandSize() {
+		return hand.getSize();
+	}
 	//takes card from hand base on index
 	public Card removeCard(int index) {
 		Card tempCard = hand.removeCard(index);
@@ -22,6 +31,27 @@ public class Player {
 		hand.addCard(card);
 	}
 	
+	//this method will check if the hand has a pair and will get rid of it
+		public void removePairs() {
+			
+			for (int i = 0; i < (hand.getSize() - 2); i++ ) {
+				Card c1 = hand.getCard(i);
+				
+				for (int j =1; j < (hand.getSize() - 1); j++) {
+					Card c2 = hand.getCard(j);
+					
+					if (Card.compareByValue.compare(c1, c2) == 0) {
+						hand.removeCard(i);
+						hand.removeCard(j);
+						System.out.println("pair removed");
+					}
+					continue;
+				}
+				continue;
+			}
+			
+			
+		}
 	
 	//toString 
 	@Override 
