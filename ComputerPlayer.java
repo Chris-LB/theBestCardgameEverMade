66cd3c8e0843b5fe999b0cb8fb575a21c141750a
Atorres2001 <64925159+Atorres2001@users.noreply.github.com>
2020-05-07 16:48:02 -0500

    used java.util.random instead of math.random

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,9 +1,11 @@
+import java.util.Random;
+
 /*
  * author alex torres
  * this class is the computerPlayer class. this class extends the player class. this 
  * class has a method that will simulate a player choosing a random card.
  */
-import java.util.ArrayList;
+
 
 public class ComputerPlayer extends Player{
 	
@@ -13,11 +15,12 @@ public class ComputerPlayer extends Player{
 	}
 	
 	//method that returns a random int value the simulates a the computer making a choice 
-	public int GenerateRandomInt (ArrayList<Card> hand) {
+	public int GenerateRandomInt () {
 		//finding the size of the hand and setting it as the max value
-		int max_value = hand.size() - 1;
-		int random = (int) (Math.random() * max_value);  
-		return random;
+		Random r = new Random();
+		int max_value = super.getHandSize();
+		int randomInt = r.nextInt(max_value);
+		return randomInt;
 	}
 
 }
